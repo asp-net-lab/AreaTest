27fb1536fe5c9cf7c0d7726b4e72b854812d8a49
ESSARRAJ Fouad <essarraj.fouad@gmail.com>
2019-02-22 10:33:32 +0100

    Create Blog Area

---
+++ AreaTest/Areas/Blogs/Controllers/PagesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AreaTest.Areas.Blogs.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Blogs/Pages
+        public ActionResult Index()
+        {
+            ViewBag.Name = "Page 1";
+            return View();
+        }
+    }
+}
