8fece17f84d65c2af2d5e41beff86edfa0d8775a
ESSARRAJ <essarraj.fouad@gmail.com>
2019-02-26 10:33:02 +0100

    save

---
+++ AreaTest/Areas/TraineesManager/TraineesManagerAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AreaTest.Areas.TraineesManager
+{
+    public class TraineesManagerAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "TraineesManager";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "TraineesManager_default",
+                "TraineesManager/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
